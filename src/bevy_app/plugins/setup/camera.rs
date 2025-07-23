c306e968dfc2bdd1baad6c931e809d3ce6dd2b46
Casey Vaughn <casey.vaughn9@aol.com>
2025-07-23 15:30:04 -0500

    fix: Camera cartesian to spherical conversion

--- src/bevy_app/plugins/setup/camera.rs
+++ src/bevy_app/plugins/setup/camera.rs
@@ -1,31 +1,34 @@
-use bevy::{
-    math::{ops::atan2, FloatPow},
-    prelude::*,
-};
+use bevy::prelude::*;
 use bevy_panorbit_camera::PanOrbitCamera;
 
 pub fn spawn_camera(mut commands: Commands) {
-    let initial_camera_position = Vec3::new(50.0, 50.0, 50.0);
-    let radius = (initial_camera_position.x.squared()
-        + initial_camera_position.y.squared()
-        + initial_camera_position.z.squared())
-    .sqrt();
-    let theta = atan2(initial_camera_position.x, initial_camera_position.y);
-    let phi = (initial_camera_position.z / radius).acos();
+    let initial = Vec3::new(8.0, 6.0, 8.0);
+    let radius = initial.length();
+    let yaw = initial.x.atan2(initial.z);
+    let pitch = (initial.y / radius).asin();
 
     commands.spawn((
         Camera3d::default(),
         PanOrbitCamera {
             focus: Vec3::ZERO,
             radius: Some(radius),
-            yaw: Some(theta),
-            pitch: Some(phi),
+            yaw: Some(yaw),
+            pitch: Some(pitch),
             ..default()
         },
     ));
 
     commands.insert_resource(AmbientLight {
         color: Color::WHITE,
-        brightness: 2000.,
+        brightness: 300.,
     });
+
+    commands.spawn((
+        DirectionalLight {
+            illuminance: 1_000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_translation(Vec3::new(15.0, 15.0, -15.0)).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
